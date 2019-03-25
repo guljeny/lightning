7127ff70b4bc4dbe3a3dd3d57c44b376aef8515a
Jenya Gul <gul.jenya@gmail.com>
2019-03-26 02:53:20 +0300

    setup demo

---
+++ demo.js
@@ -0,0 +1,6 @@
+import Lightning from "./src/index.js"
+
+new Lightning(document.querySelector("#l1"), {})
+new Lightning(document.querySelector("#l2"), {})
+new Lightning(document.querySelector("#l3"), {})
+
